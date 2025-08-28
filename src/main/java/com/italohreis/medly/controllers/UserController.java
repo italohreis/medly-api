0ae0c0f51d105dc512ab894654d80ef4789de12f
Italo Reis <italohenrikbr@gmail.com>
2025-08-28 10:07:12 -0300

    feat: implement password change functionality with validation

--- src/main/java/com/italohreis/medly/controllers/UserController.java
+++ src/main/java/com/italohreis/medly/controllers/UserController.java
@@ -1,14 +1,15 @@
 package com.italohreis.medly.controllers;
 
+import com.italohreis.medly.dtos.user.UserPasswordUpdateDTO;
 import com.italohreis.medly.dtos.user.UserProfileResponseDTO;
 import com.italohreis.medly.services.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -21,5 +22,13 @@ public class UserController {
     @PreAuthorize("isAuthenticated()")
     public ResponseEntity<UserProfileResponseDTO> getMyProfile(Authentication authentication) {
         return ResponseEntity.ok(userService.getUserProfile(authentication));
+    }
+
+    @PatchMapping("/me/password")
+    @PreAuthorize("isAuthenticated()")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void changeMyPassword(Authentication authentication,
+                                 @RequestBody @Valid UserPasswordUpdateDTO dto) {
+        userService.changeUserPassword(authentication, dto);
     }
 }
--- src/main/java/com/italohreis/medly/services/UserService.java
+++ src/main/java/com/italohreis/medly/services/UserService.java
@@ -1,11 +1,14 @@
 package com.italohreis.medly.services;
 
+import com.italohreis.medly.dtos.user.UserPasswordUpdateDTO;
 import com.italohreis.medly.dtos.user.UserProfileResponseDTO;
 import com.italohreis.medly.enums.Role;
+import com.italohreis.medly.exceptions.BusinessRuleException;
 import com.italohreis.medly.exceptions.EmailAlreadyExistsException;
 import com.italohreis.medly.mappers.UserMapper;
 import com.italohreis.medly.models.User;
 import com.italohreis.medly.repositories.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -42,5 +45,22 @@ public class UserService {
         user.setPassword(passwordEncoder.encode(password));
         user.setRole(role);
         return user;
+    }
+
+    @Transactional
+    public void changeUserPassword(Authentication authentication, UserPasswordUpdateDTO dto) {
+        if (!dto.newPassword().equals(dto.confirmPassword())) {
+            throw new BusinessRuleException("New password and confirmation password do not match.");
+        }
+
+        String userEmail = (String) authentication.getPrincipal();
+        User currentUser = userRepository.findByEmailAndActiveTrue(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        if (!passwordEncoder.matches(dto.currentPassword(), currentUser.getPassword())) {
+            throw new BusinessRuleException("Incorrect current password.");
+        }
+
+        currentUser.setPassword(passwordEncoder.encode(dto.newPassword()));
     }
 }
