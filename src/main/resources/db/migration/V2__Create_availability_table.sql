f019d8afe908a7d150dd4736e48cfc427d4fdb4d
Italo Reis <italohenrikbr@gmail.com>
2025-08-29 12:33:27 -0300

    feat: add Docker support with Dockerfile and docker-compose.yml  and create initial database schema and data

--- src/main/resources/db/migration/V2__Create_availability_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE availability (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    doctor_id UUID NOT NULL REFERENCES doctor(id),
-    start_time TIMESTAMP NOT NULL,
-    end_time TIMESTAMP NOT NULL,
-    is_available BOOLEAN NOT NULL DEFAULT TRUE
-);
--- src/main/resources/db/migration/V4__Create_status_column_availability.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE availability ADD COLUMN status VARCHAR(255);
-
-UPDATE availability SET status = 'BOOKED' WHERE is_available = false;
-UPDATE availability SET status = 'AVAILABLE' WHERE is_available = true;
-
-ALTER TABLE availability DROP COLUMN is_available;
-
